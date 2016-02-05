1c2a62880c7d2ae81c439a191256843cd678bd37
Werner Kroneman <werner@wernerkroneman.nl>
2016-02-05 15:45:12 +0100

    More Linux fixes.

--- src/geomeshd/TunnelDeliveryInterface_Linux.cpp
+++ src/geomeshd/TunnelDeliveryInterface_Linux.cpp
@@ -6,14 +6,8 @@
 
 #include "../Logger.h"
 
-#include <sys/if_tun.h>
-#include <sys/ioctl.h>
-#include <net/if.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/ipv6.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
 
 void TunnelDeliveryInterface_Linux::startTunnelInterface() {
 
