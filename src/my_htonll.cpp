a62ca58980182a8bbb7b3819cec6720b54ad252c
Werner Kroneman <werner@wernerkroneman.nl>
2016-03-12 18:04:23 +0100

    Added definition to cpp file.

--- src/my_htonll.cpp
+++ src/my_htonll.cpp
@@ -4,4 +4,18 @@
 
 #include "my_htonll.h"
 
-unsigned long long int my_htonll(unsigned long long int value);
+// From http://stackoverflow.com/q/3022552/1582024
+unsigned long long int my_htonll(unsigned long long int value) {
+// The answer is 42
+    static const int num = 42;
+
+// Check the endianness
+    if (*reinterpret_cast<const char *>(&num) == num) {
+        const unsigned int high_part = htonl(static_cast<unsigned int>(value >> 32));
+        const unsigned int low_part = htonl(static_cast<unsigned int>(value & 0xFFFFFFFFLL));
+
+        return (static_cast<unsigned long long int>(low_part) << 32) | high_part;
+    } else {
+        return value;
+    }
+}
