fbab9a6d9d1dfb5462d2a94f3710b5de54ad6e07
Werner Kroneman <werner@wernerkroneman.nl>
2016-04-05 18:31:44 +0200

    Wrote the ContactsSet container to keep track of a list of contacts and query them efficiently.

---
+++ src/ContactsSet.cpp
@@ -0,0 +1,89 @@
+//
+// Created by Werner Kroneman on 04-04-16.
+//
+
+#include "ContactsSet.hpp"
+
+ContactsSet::const_iterator ContactsSet::findClosestEntry(const Address& query) const {
+
+    auto a = entries.begin(); // Take an iterator to begin
+
+    if (entries.empty()) {
+        return entries.end();
+    }
+
+    auto b = entries.end() - 1; // And to end.
+
+    int match = 0; // The bit position to test to check for prefix equality
+
+    while (a < b) {
+
+        int bitA = a->address.getBit(match);
+        int bitB = b->address.getBit(match);
+
+        if (bitA == 0 && bitB == 1) {
+            // Find split point through binary search
+            auto c = a;
+            auto d = b;
+
+            int dist;
+
+            while ((dist = std::distance(c,d)) > 1) {
+
+                auto middle = c + dist / 2; // Take average.
+
+                if (middle->address.getBit(match) == 0) {
+                    c = middle;
+                } else { // Must be 1 by excluded middle
+                    d = middle;
+                }
+            }
+
+            if (query.getBit(match) == 0) {
+                b = c;
+            } else {
+                a = d;
+            }
+        }
+        match += 1;
+    }
+
+    return a;
+}
+
+ContactsSet::iterator ContactsSet::insert(const Entry& entry) {
+
+    // http://stackoverflow.com/a/31294973/1582024
+
+    if (entries.empty()) {
+        return entries.insert(entries.end(), entry);
+        // Otherwise we get some really wierd behavior with iterators moving before the begin.
+    }
+
+    auto first = entries.begin();
+    auto last = entries.end() - 1;
+
+    while (first < last) {
+
+        bool equal = first > last;
+
+        int distance = std::distance(first,last);
+        auto mid = first + distance/2;
+
+        if (mid->address == entry.address) {
+            *mid = entry;
+            return mid;
+        } else if (mid->address > entry.address) {
+            last = mid - 1;
+        } else {
+            first = mid + 1;
+        }
+    }
+    if (first == entries.end()) {
+        return entries.insert(entries.end(), entry);
+    } else if(first->address > entry.address) {
+        return entries.insert(first, entry);
+    } else {
+        return entries.insert(first + 1, entry);
+    }
+}
---
+++ src/ContactsSet.hpp
@@ -0,0 +1,73 @@
+//
+// Created by Werner Kroneman on 04-04-16.
+//
+
+#ifndef GEOMESH_CONTACTSSET_H
+#define GEOMESH_CONTACTSSET_H
+
+#include "UniqueAddress.hpp"
+
+#include "Location.hpp"
+
+#include "my_htonll.hpp"
+
+class ContactsSet {
+
+public:
+    struct Entry {
+
+        Address address;
+        Location location;
+        time_t expires;
+
+        const static size_t SERIALIZED_SIZE = ADDRESS_LENGTH_OCTETS + Location::SERIALIZED_SIZE + 8;
+
+        Entry(const Address& addr, const Location& loc, const time_t& expires) :
+                address(addr), location(loc), expires(0) {}
+
+        Entry(const uint8_t* bytes) :
+                address(Address::fromBytes(bytes)),
+                location(Location::fromBytes(bytes+ADDRESS_LENGTH_OCTETS))
+        {
+            expires = my_htonll(* reinterpret_cast<const uint64_t*>(bytes +
+                    ADDRESS_LENGTH_OCTETS + Location::SERIALIZED_SIZE));
+        }
+
+        void toBytes(uint8_t* buffer) {
+            memcpy(buffer, address.getBytes(), ADDRESS_LENGTH_OCTETS);
+            location.toBytes((uint8_t*)buffer + ADDRESS_LENGTH_OCTETS);
+            *(reinterpret_cast<uint64_t*>(buffer + ADDRESS_LENGTH_OCTETS + 8)) = my_htonll(expires);
+        }
+    };
+
+private:
+    /*
+     * Rep invariant: entries must always be sorted by address
+     */
+    std::vector<Entry> entries;
+
+public:
+    typedef std::vector<Entry>::iterator iterator;
+    typedef std::vector<Entry>::const_iterator const_iterator;
+    iterator begin() { return entries.begin(); }
+    const_iterator begin() const { return entries.begin(); }
+    iterator end() { return entries.end(); }
+    const_iterator end() const { return entries.end(); }
+
+    /**
+     * Find the contact whose address most closely matches the query
+     * using the xor distance metric.
+     */
+    const_iterator findClosestEntry(const Address& query) const;
+
+    iterator insert(const Entry& entry);
+
+    bool empty() const {
+        return entries.empty();
+    }
+
+    void recomputeSharedPrefix();
+};
+
+
+#endif //GEOMESH_CONTACTSSET_H
