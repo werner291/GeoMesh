acb8874e5564d57df8c3fbcf5fdbb2ebbd85cf25
Werner Kroneman <werner@wernerkroneman.nl>
2016-04-17 13:38:53 +0200

    Added boost libraries to cmakelists, made the Logger apply a strategy such that logging can be changed at will.

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -8,9 +8,10 @@
 
 LogLevel Logger::minLevel = LogLevel::INFO;
 
-void Logger::log(LogLevel level, std::string message) {
+// Default log strategy is just to prnt to standard out.
+LogStrategy Logger::log = [](LogLevel level, std::string message) {
 
     if (level >= minLevel) {
         std::cout << std::to_string(std::time(NULL)) << ": " << message << std::endl;
     }
-}
+};
--- src/Logger.hpp
+++ src/Logger.hpp
@@ -6,6 +6,7 @@
 #define MESHNETSIM_LOGGER_H
 
 #include <string>
+#include <functional>
 
 enum LogLevel {
     DEBUG = 0,
@@ -14,14 +15,34 @@ enum LogLevel {
     ERROR = 3
 };
 
+typedef std::function<void(const LogLevel& level, const std::string& message)> LogStrategy; 
+
+/**
+ * Logger is a static-only class that handles all things related to logging.
+ * Call Logger::log() to log something.
+ *
+ * Singletons and global variables are generally against the GeoMesh codestyle,
+ * but since logging is so omnipresent, globally-accessible logging will have
+ * to be a necessary evil. (Does anyone have a better idea?)
+ *
+ * How logging is to be handled can be set by calling setLogStrategy().
+ * By default, things are just sent to standard output, but client code may
+ * for example change this so that files are written to a log server.
+ */
 class Logger {
     static LogLevel minLevel;
 public:
     static void setLogLevel(LogLevel level) {
 	    minLevel = level;
     }
-    static void log(LogLevel level, std::string message);
+
+    static void setLogStrategy(const LogStrategy& strategy) {
+        Logger::log = strategy;
+    }
+
+    static LogStrategy log;
 };
 
 
 #endif //MESHNETSIM_LOGGER_H
+
