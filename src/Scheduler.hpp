34d851e45270a903eb44f8b89cd010a69180f521
Werner Kroneman <werner@wernerkroneman.nl>
2016-04-29 16:11:41 +0200

    Added documentation and license headers.

--- src/Scheduler.hpp
+++ src/Scheduler.hpp
@@ -1,6 +1,21 @@
-//
-// Created by Werner Kroneman on 06-04-16.
-//
+/*
+ * (c) Copyright 2016 Werner Kroneman
+ *
+ * This file is part of GeoMesh.
+ * 
+ * GeoMesh is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * GeoMesh is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with GeoMesh.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef GEOMESH_SCHEDULER_H
 #define GEOMESH_SCHEDULER_H
@@ -12,6 +27,17 @@
 #include <thread>
 #include <assert.h>
 
+/**
+ * Scheduler is a class responsible for timing-related functionality.
+ *
+ * Client code can register tasks to be executed at specific times
+ * and intervals, and these will be called when needed.
+ *
+ * Usage:
+ *    Call scheduleTask() to add a new task.
+ * If using synchronously, the update() function must be called at a regular interval.
+ * If using async, the callbacks will be called from another thread.
+ */
 class Scheduler {
 
 public:
@@ -24,11 +50,20 @@ public:
     typedef clock::duration duration;
     typedef std::function<void (time_point currentTime, duration delta, Task& task)> TaskCallback;
 
+    /**
+     * Struct representing a task.
+     */
     struct Task {
+        // When to next execute the task. Task will be executed at first opportunity after time point has passed.
         time_point planned;
+
+        // For repeating tasks, a duration after which to reschedule the task.
         duration interval;
+
+        // Whether the task should be automatically re-scheduled after being run.
         bool repeats;
 
+        // Callback to call when the planned time point has passed.
         TaskCallback callback;
 
         Task(time_point planned, duration interval, bool repeats, TaskCallback callback)
@@ -36,6 +71,7 @@ public:
 
         }
 
+        // For repeating tasks, call this method and it won't be repeated anymore.
         void cancel() {
             repeats = false;
         }
@@ -45,6 +81,7 @@ private:
 
     std::vector<Task> tasks;
 
+    // The last time when  update() was last called.
     time_point lastUpdate;
 
     // Some clients might want to start or stop tasks from the callback, make that safe to do.
